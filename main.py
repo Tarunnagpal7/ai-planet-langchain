3dca530eaae79eec76cb6c40ef50647eb1f31efe
tarunnagpal7 <tarunnagpal192004@gmail.com>
2025-04-27 12:53:16 +0530

    added env port

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from fastapi.middleware.cors import CORSMiddleware
 import logging
 
 from routers import upload, qa
+import os
+from dotenv import load_dotenv
 
 
-
+load_dotenv()
 
 logging.basicConfig(
     level=logging.INFO,
@@ -40,4 +42,5 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
